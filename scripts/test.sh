d096e70b51a3f8f2855cf314cf4b1ced92a810e9
lambdalisue <lambdalisue@hashnote.net>
2016-10-29 20:50:01 +0900

    Remove docs from ci-test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-if [[ ! -d 'prompt' ]]; then
-  echo 'A "prompt" directory is missing.' >&2
-  exit 1
+set -e
+if [[ -d 'prompt' ]]; then
+  rm -rf prompt
+fi
+
+if [[ -f '../prompt.py' ]]; then
+  mkdir prompt
+  for file in $(ls -1v ../*.py*); do
+    cp $file prompt/
+  done
+else
+  git clone --single-branch --depth 1 \
+    https://github.com/lambdalisue/neovim-prompt prompt
 fi
 pip install -qr requirements-test.txt
 pytest
